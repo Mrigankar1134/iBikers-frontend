14572df91723c98eb8ede160cb6f3da90ae0fbe7
Mrigankar Sonowal <mrigankarofficial@gmail.com>
2025-06-20 15:13:50 +0530

    Logo Smaller

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.scss';
-import Logo from '../../public/Logo.png';
+import Logo from '../../public/logo.png';
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
